b7d7fda1527cb613366ea18ac455a176eb9c8d20
Iurii <Iurii>
2023-11-22 10:39:31 +0500

    Initial Commit

---
+++ New_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New_project
+//
+//  Created by Юрий Емелин on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
